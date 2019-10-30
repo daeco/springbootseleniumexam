2cbd6d8d7bd85515994b15ed7933380324420651
daeco <rlaeogh89@gmail.com>
2019-10-30 12:39:15 +0900

    [selenium exam] 로깅 추가 및 텔레그램 봇 추가

--- src/main/java/com/springboot/selenium/exam/Application.java
+++ src/main/java/com/springboot/selenium/exam/Application.java
@@ -1,22 +1,47 @@
+package com.springboot.selenium.exam;
+
+import com.springboot.selenium.exam.util.MyBot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.List;
 
-public class Main {
+@SpringBootApplication
+public class Application {
+    private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
-    public static final String WEB_DRIVER_PATH = "D:/Big Tiger/dev/chromedriver_win32/chromedriver.exe";
+    public static final String WEB_DRIVER_PATH = "C:/dev/chromedriver_win32/chromedriver.exe";
     public static final String BASE_URL = "https://swexpertacademy.com/main/sst/intro.do";
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        MyBot myBot = new MyBot();
+        try {
+            botsApi.registerBot(myBot);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
         ChromeOptions options = new ChromeOptions();
         options.setCapability("ignoreProtectedModeSettings", true);
+        options.addArguments("headless");
         WebDriver driver = new ChromeDriver(options);
 
         try {
@@ -39,21 +64,26 @@ public class Main {
             WebElement btn_next_month = driver.findElement(By.className("ui-datepicker-next"));
             btn_next_month.click();
             List<WebElement> ableDay = driver.findElements(By.className("ableDaySaturday"));
-            ableDay.get(0).click();
+
+            JavascriptExecutor executor = (JavascriptExecutor)driver;
+            executor.executeScript("arguments[0].click();", ableDay.get(0));
 
             while(true) {
                 WebElement selectedDay = driver.findElement(By.className("selectedDay"));
-                selectedDay.click();
+                executor.executeScript("arguments[0].click();", selectedDay);
+
 
                 List<WebElement> certi_time = driver.findElements(By.className("certi-time"));
                 certi_time.get(0).findElement(By.tagName("a")).click();
 
                 List<WebElement> btn_complete = driver.findElements(By.className("btn-complete"));
-                //System.out.println(btn_complete.size());
 
                 if(btn_complete.size() < 2) {
                     // push message
-                    System.out.println("자리있다!");
+                    SendMessage sn = new SendMessage();
+                    sn.setChatId("1042595863");
+                    sn.setText("SW 역량테스트 A형 신청 가능!");
+                    myBot.execute(sn);
                     break;
                 }
                 else {
@@ -65,8 +95,10 @@ public class Main {
                         }
                     }
                     if(isEnd) {
-                        // push message
-                        System.out.println("자리있다!");
+                        SendMessage sn = new SendMessage();
+                        sn.setChatId("1042595863");
+                        sn.setText("SW 역량테스트 A형 신청 가능!");
+                        myBot.execute(sn);
                         break;
                     }
                 }
@@ -74,11 +106,18 @@ public class Main {
                 Thread.sleep(2000);
             }
         } catch (Exception e) {
+            LOGGER.error("Unexpected problem encountered", e);
+            SendMessage sn = new SendMessage();
+            sn.setChatId("1042595863");
+            sn.setText("오류로 인한 프로그램 종료");
+            try {
+                myBot.execute(sn);
+            } catch (TelegramApiException te) {
+                te.printStackTrace();
+            }
 
         } finally {
             driver.close();
         }
-
-
     }
 }
---
+++ src/main/java/com/springboot/selenium/exam/util/MyBot.java
@@ -0,0 +1,32 @@
+package com.springboot.selenium.exam.util;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MyBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText("대답할 수 없습니다.");
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "ari0920_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1050810032:AAFOwiXtTtYe8GBaOuGcoanw4N2m0jV7cy8";
+    }
+}
